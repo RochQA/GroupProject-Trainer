fff89a108d4be7884f48404a3fb0d1fd8fa9336a
Roch <roch.sherriff@academytrainee.com>
2019-04-01 15:02:56 +0100

    eureka

--- src/main/java/com/qa/trainer/TrainerApplication.java
+++ src/main/java/com/qa/trainer/TrainerApplication.java
@@ -2,8 +2,10 @@ package com.qa.trainer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TrainerApplication {
 
 	public static void main(String[] args) {
